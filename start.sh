756effae1cf3dfe43f5b4173854008d6d6c768b4
jereminius <attiii6@gmail.com>
2023-05-17 12:17:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-# Select the crontab file based on the environment
-CRON_FILE="crontab"
+# Define the cron schedule based on the SCHEDULE environment variable
+case "$SCHEDULE" in
+  "daily")
+    CRON_EXPRESSION="0 1 * * *"
+    ;;
+  "weekly")
+    CRON_EXPRESSION="0 1 * * 1"
+    ;;
+  "monthly")
+    CRON_EXPRESSION="0 1 1 * *"
+    ;;
+  *)
+    CRON_EXPRESSION="$SCHEDULE"
+    ;;
+esac
 
-echo "Loading crontab file: $CRON_FILE"
+# Set the cron schedule and command
+CRON_JOB="$CRON_EXPRESSION /usr/loxone_scheduler/loxone_backup.sh"
 
-# Load the crontab file
-crontab $CRON_FILE
+# Write the cron job to a temporary file
+echo "$CRON_JOB" > /tmp/cron_job
 
-echo "Starting cron..."
+# Load the cron job from the temporary file
+crontab /tmp/cron_job
 
-# Start cron
-crond -f
+# Remove the temporary file
+rm /tmp/cron_job
+
+# Start cron in the foreground
+echo "[$(date +"%a %b %d %T %Y")] Starting cron..."
+exec crond -f
